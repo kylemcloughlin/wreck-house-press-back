1eab3196afb2f0acf9d6bc8f2d67b0f00fa2ae66
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-07 21:04:45 -0500

    commented out secure passowrd and cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,17 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-  allow do
-    origins 'https://wreck-house-press-front2.vercel.app'
-    resource "*",
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
 
