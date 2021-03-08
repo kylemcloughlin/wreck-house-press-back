48673b1160de2d00acc36436487133d878391b06
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-07 21:35:53 -0500

    found  typo in sessions controller? it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
- include currenUserConcern 
+ include currentUserConcern 
   def create
     user = User.find_by(email: params['user']["email"]).try(:authenticate, params["user"]["password"])
     if user
