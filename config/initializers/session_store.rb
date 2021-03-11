ce9bc6be162e3437e50ce84d54b2258351ca515e
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-11 15:37:55 -0500

    commented out session store local

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: "_wreck_house_press_back_session", domain: "wreck-house-press-back.herokuapp.com"
 
 
-# if Rails.env == 'production'
-#   Rails.application.config.session_store :cookie_store, key: "_wreck_house_press_back_session",  domain: "wreck-house-press-back.herokuapp.com"
-# else 
-#   Rails.application.config.session_store :cookie_store, key: "_wreck_house_press_back_session", expire_after: 14.hours
-# end
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: "_wreck_house_press_back_session",  domain: "wreck-house-press-back.herokuapp.com"
+else 
+  Rails.application.config.session_store :cookie_store, key: "_wreck_house_press_back_session", expire_after: 14.hours
+end
