58375d67cd652dc930635ab93706c9a5b554f5a6
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-11 14:25:23 -0500

    updated url on session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_wreck_house-press-back_session",  domain: "https://wreck-house-press-back.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: "_wreck_house-press-back_session",  domain: "https://wreck-house-press-back.herokuapp.com"
 else 
   Rails.application.config.session_store :cookie_store, key: "_wreck_house_press_back_session", expire_after: 14.hours
 end
