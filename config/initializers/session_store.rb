4a88e01f1eae676f4ac749f27907d7c6df809a32
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-11 17:46:27 -0500

    domain :all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 
 
-if Rails.env == 'production'
+if Rails.env.production?
   Rails.application.config.session_store :cookie_store, key: "_session_id",  domain: :all
 else 
   Rails.application.config.session_store :cookie_store, key: "_session_id", expire_after: 14.hours
