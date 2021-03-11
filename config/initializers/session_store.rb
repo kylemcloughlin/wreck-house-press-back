ea5e82aa6f5d55551f672db5a69a7649d3d37ca1
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-11 18:34:56 -0500

    i think i solved it deleted session_store

--- config/initializers/session_store.rb
+++
@@ -1,7 +0,0 @@
-
-
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: "_session_id", domain: '.wreck-house-press-back.herokuapp.com'
-else 
-  Rails.application.config.session_store :cookie_store, key: "_session_id"
-end
