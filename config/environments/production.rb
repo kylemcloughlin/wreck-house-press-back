df558da6e75dbdaee31130eb13d5a8fbac3a528f
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-11 16:30:27 -0500

    commented out session store local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
   config.force_ssl = true
-WreckHousePressBack::Application.config.session_store :cookie_store, :key => "_session_id", :domain => :all
+# WreckHousePressBack::Application.config.session_store :cookie_store, :key => "_session_id", :domain => :all
 
 
   # Use the lowest log level to ensure availability of diagnostic information
