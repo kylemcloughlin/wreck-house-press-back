6958a6919f71abd6cb4cd3a74ae6b04a8964ffbd
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-12 11:15:44 -0500

    i just deleted the domain

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem "activerecord-session_store"
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- config/application.rb
+++ config/application.rb
@@ -25,10 +25,14 @@ module WreckHousePressBack
     config.load_defaults 6.0
 
 # config.middleware.use config.session_store, config.session_options
-    config.session_store :cookie_store, key: "_session_id", domain: "wreck-house-press-back.herokuapp.com"
-    config.middleware.use ActionDispatch::Cookies 
-    config.middleware.use ActionDispatch::Session::CookieStore
-    config.middleware.use config.session_store, config.session_options
+    config.session_store :cookie_store, key: "_session_id"
+  #  Rails.application.config.session_store :active_record_store, :key => "_my_app_session"
+  config.middleware.use ActionDispatch::Cookies 
+  # config.middleware.use ActionDispatch::Session::ActiveRecordStore
+      config.middleware.use ActionDispatch::Session::CookieStore
+     config.middleware.use config.session_store, config.session_options
+
+
     # config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
 
     # Settings in config/environments/* take precedence over those specified here.
