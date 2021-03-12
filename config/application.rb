000525817e374a915fd0e3f6298227c729ad02a4
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-12 12:04:16 -0500

    pls GOD

--- config/application.rb
+++ config/application.rb
@@ -26,14 +26,14 @@ module WreckHousePressBack
 
 # config.middleware.use config.session_store, config.session_options
 if Rails.env.production?
-  config.session_store :cookie_store, key: "_session_id", domain: ["wreck-house-press-back.herokuapp"]
+  Rails.application.config.session_store :cookie_store, key: "_session_id", domain: ["wreck-house-press-back.herokuapp"]
   
 else
-  config.session_store :cookie_store, key: "_session_id"
+   Rails.application.config.session_store :cookie_store, key: "_session_id"
 end    
 
 
-   Rails.application.config.session_store :active_record_store, :key => "_my_app_session"
+  #  Rails.application.config.session_store :active_record_store, :key => "_my_app_session"
   config.middleware.use ActionDispatch::Cookies 
   # config.middleware.use ActionDispatch::Session::ActiveRecordStore
       config.middleware.use ActionDispatch::Session::CookieStore
