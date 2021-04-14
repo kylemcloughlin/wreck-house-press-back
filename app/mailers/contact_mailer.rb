0f0d894b8ca403e68ee24d86274c6357df425c5d
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-04-13 22:41:45 -0400

    changed mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,24 +3,24 @@ class ContactMailer < ApplicationMailer
   def send_about_email
       @email = params[:email]
      @email_body = params[:email_body]
-  mail(to: "wreckhousepress.utility@gmail.com",
+  mail(to: "info@wreckhousepress.com",
        subject: "General Inquiry",
-       bcc: ["kylemcloughlin00@gmail.com"])
+       bcc: ["wreckhousepress.utility@gmail.com"])
   end
 
   def send_publishing_email
      @email = params[:email]
      @email_body = params[:email_body]
-  mail(to: 'wreckhousepress.utility@gmail.com',
+  mail(to: 'info@wreckhousepress.com',
        subject: "Publishing Inquiry",
-       bcc: ["kylemcloughlin00@gmail.com"])
+       bcc: ["wreckhousepress.utility@gmail.com"])
   end
   def send_advertising_email
        @email = params[:email]
        @email_body = params[:email_body]
-  mail(to: "wreckhousepress.utility@gmail.com",
+  mail(to: "info@wreckhousepress.com",
        subject: "Advertising Inquiry",
-       bcc: ["kylemcloughlin00@gmail.com"])
+       bcc: ["wreckhousepress.utility@gmail.com"])
 end
 end
 
