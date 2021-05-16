a6ffacc26f2d14012850119b681c698c8b53774c
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-05-16 11:16:39 -0400

    adding login changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => 'smtp.gmail.com',
    :port                 => 587,
-    :user_name            => ENV['USER_NAME'],
+      :user_name            => ENV['USER_NAME'],
       :password             => ENV['EMAIL_PASS'],
       :authentication       => 'login',
   }
